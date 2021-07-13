9159763d660b0e15c2ea74f9fcf5bb84517cc919
Anthony Salamin <37147870+anthonysalamin@users.noreply.github.com>
2021-07-13 18:23:41 +0200

    first frontend commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,13 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Gatsby site with strapi deployed on render",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: ["gatsby-plugin-gatsby-cloud", {
+    resolve: `gatsby-source-strapi`,
+    options: {
+      apiURL: `http://localhost:1337`,
+      queryLimit: 1000, // Defaults to 100
+      collectionTypes: [`article`, `user`],
+      singleTypes: [],
+    },
+  },],
 };
