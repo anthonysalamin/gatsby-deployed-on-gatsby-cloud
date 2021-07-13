31bfbacf55db714ea515715e42d2d96d20a51f77
Anthony Salamin <37147870+anthonysalamin@users.noreply.github.com>
2021-07-13 16:35:05 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Gatsby site with strapi deployed on render",
+  },
+  plugins: ["gatsby-plugin-gatsby-cloud"],
+};
